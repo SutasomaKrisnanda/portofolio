2f5b0181964b84e335322bb9984a85dc3716a345
sutasomakrisnanda <sutasomakrisnanda@gmail.com>
2026-04-01 10:21:08 +0800

    Add basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/portofolio',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
