a34ebd0904993a2f2e53f843345ec5e801a7129c
sutasomakrisnanda <sutasomakrisnanda@gmail.com>
2026-04-01 11:20:25 +0800

    Remove basePath configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portofolio',
+  // basePath: '/portofolio',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
